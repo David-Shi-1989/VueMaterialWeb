2a3873f417c53fc54a19c2ad730f656c1bea4361
David_Shi <287841605@qq.com>
2021-01-21 15:03:50 +0800

    Create common.ts

---
+++ src/i18n/common.ts
@@ -0,0 +1,40 @@
+const commonCN = {
+  username: '用户名',
+  password: '密码',
+  language: '语言',
+  langEN: '英文',
+  langCN: '中文',
+  login: '登录',
+  logout: '退出',
+  ok: '确认',
+  cancel: '取消',
+  confirm: '请确认',
+  theme: '主题',
+  tenant: '租户',
+  about: '关于',
+  help: '联机帮助',
+  themeDark: '深色',
+  themeLight: '浅色'
+}
+const commonEN = {
+  username: 'UserName',
+  password: 'Password',
+  language: 'Language',
+  langEN: 'English',
+  langCN: 'Chinese',
+  login: 'Login',
+  logout: 'Logout',
+  ok: 'OK',
+  cancel: 'Cancel',
+  confirm: 'Please Confirm',
+  theme: 'Theme',
+  tenant: 'Tenant',
+  about: 'About',
+  help: 'Help Docs',
+  themeDark: 'Dark',
+  themeLight: 'Light'
+}
+
+export {
+  commonCN, commonEN
+}
