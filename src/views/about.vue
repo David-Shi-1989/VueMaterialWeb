c763ad9458c8eda0e89089be30b504da345e68a5
David_Shi <287841605@qq.com>
2021-01-21 15:09:37 +0800

    Create about.vue

---
+++ src/views/about.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h3>About page</h3>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+<style lang="css" scoped>
+h3 {
+  color: red;
+}
+</style>
