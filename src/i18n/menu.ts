16358e7183f4e89021eb54b60c0ba8afb4be2456
David_Shi <287841605@qq.com>
2021-01-21 15:04:42 +0800

    Create menu.ts

---
+++ src/i18n/menu.ts
@@ -0,0 +1,47 @@
+const menuCN = {
+  dashboard: '首页',
+  'apply-network': '应用网络',
+  infrastructure: '基础设施',
+  'net-controller': '网络控制器',
+  fabric: 'Fabric',
+  device: '设备',
+  dpi: 'DPI库',
+  parameters: '参数',
+  app: '安全服务',
+  tenants: '@:(m.common.tenant)',
+  tenantAll: '全部租户',
+  resources: '资源',
+  resourceService: '服务资源',
+  services: '服务',
+  firewall: '防火墙',
+  assurance: '保障',
+  overview: '概览',
+  'controller-info': '控制器信息',
+  'system-log': '日志信息',
+  settings: '设置'
+}
+const menuEN = {
+  dashboard: 'Index',
+  'apply-network': 'Apply Network',
+  infrastructure: 'Infrastructure',
+  'net-controller': 'Net Controller',
+  fabric: 'Fabric',
+  device: 'Device',
+  dpi: 'DPI Lib',
+  parameters: 'Parameters',
+  app: 'Application',
+  tenants: 'Tenants',
+  tenantAll: 'All Tenants',
+  resources: 'Resources',
+  resourceService: 'Service',
+  services: 'Services',
+  firewall: 'Firewall',
+  assurance: 'Assurance',
+  overview: 'Overview',
+  'controller-info': 'Controller Info',
+  'system-log': 'System Log',
+  settings: 'Settings'
+}
+export {
+  menuCN, menuEN
+}
