a3d241242f43d6ddb592b346bac9e10ee7e4ce08
David_Shi <287841605@qq.com>
2021-01-21 15:06:20 +0800

    Create material-import.ts

---
+++ src/script/material-import.ts
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import 'vue-material/dist/vue-material.min.css'
+import { MdAvatar, MdButton, MdBadge, MdField, MdMenu, MdList, MdDialog, MdDrawer, MdToolbar } from 'vue-material/dist/components'
+
+Vue.use(MdAvatar)
+Vue.use(MdButton)
+Vue.use(MdBadge)
+Vue.use(MdField)
+Vue.use(MdMenu)
+Vue.use(MdList)
+Vue.use(MdDialog)
+Vue.use(MdDrawer)
+Vue.use(MdToolbar)
