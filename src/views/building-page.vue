3897f8f2bde5e23becd6cd5b0018fe2a233cbacb
David_Shi <287841605@qq.com>
2021-01-21 15:09:51 +0800

    Create building-page.vue

---
+++ src/views/building-page.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h3>{{pageTitle}} - 页面建设中</h3>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+  computed: {
+    pageTitle () {
+      return this.$route.meta.title
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+</style>
